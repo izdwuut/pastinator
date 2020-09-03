3428fc26b9026ef5dba4dba416539aca927179b7
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-03 10:44:27 +0200

    Add paste page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddPasteComponent } from './add-paste/add-paste.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule, } from '@angular/material/datepicker';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, DateAdapter } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import * as _moment from 'moment';
+import { MomentDateAdapter, MatMomentDateModule } from '@angular/material-moment-adapter';
+
+const moment = _moment;
+
+export const DATE_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+    locale: 'en-GB'
+  },
+  display: {
+    dateInput: 'YYYY-MM-DD',
+    monthYearLabel: 'YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'YYYY',
+
+  },
+  week: {
+    dow: 1
+  }
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPasteComponent,
   ],
   imports: [
+    AppRoutingModule,
+    MatDatepickerModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatMomentDateModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: DATE_FORMATS },
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
+  ],
+  bootstrap: [
+    AppComponent,
+  ]
 })
 export class AppModule { }
