e3be24abfaa542d91914b0fa83cef2a175f7014e
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-03 07:12:31 +0200

    Add routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddPasteComponent } from './add-paste/add-paste.component'
+import { AppComponent } from './app.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AddPasteComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppRoutingModule { }
