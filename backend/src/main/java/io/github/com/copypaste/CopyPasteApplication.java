b96808e5715bd3365a85ece84bf47f08dd50df45
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-04 06:35:25 +0200

    Add Spring Boot project.

---
+++ backend/src/main/java/io/github/com/copypaste/CopyPasteApplication.java
@@ -0,0 +1,13 @@
+package io.github.com.copypaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyPasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyPasteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/com/copypaste/CopyPasteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.copypaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyPasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
