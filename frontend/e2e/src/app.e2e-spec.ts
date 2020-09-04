8e188bfeb3c60e18862d75394bbec2984d65ecc5
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-04 07:00:07 +0200

    Rename project from copypaste to pastinator.

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('copypaste app is running!');
+    expect(page.getTitleText()).toEqual('pastinator app is running!');
   });
 
   afterEach(async () => {
