b84fcff49b9599c729d5d8752fd5fb527d85eb8b
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-06 03:22:30 +0200

    Format paste white characters.

--- frontend/src/app/add-paste/add-paste.component.ts
+++ frontend/src/app/add-paste/add-paste.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, APP_ID } from '@angular/core';
+import { Component, OnInit, APP_ID, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { RestService } from '../services/rest.service'
 import { Paste } from '../entities/paste';
 import { Router } from '@angular/router';
@@ -9,22 +9,26 @@ import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './add-paste.component.html',
   styleUrls: ['./add-paste.component.scss']
 })
-export class AddPasteComponent implements OnInit {
+export class AddPasteComponent implements OnInit, AfterViewInit {
   title: String
-  paste: String
+  paste: String = ''
   author: String
   expires: Date
   error: String
+  @ViewChild('pasteInput') pasteInput: ElementRef;
 
-  constructor(private api: RestService, private router: Router,private _snackBar: MatSnackBar) {}
+  constructor(private api: RestService, private router: Router, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void { 
   }
 
   addPaste(): void {
     const addedPaste: Paste = new Paste(null, null, this.paste, this.author, this.title, this.expires)
     this.api.addPaste(addedPaste).subscribe(receivedPaste => {
-      if(receivedPaste.id) {
+      if (receivedPaste.id) {
         this.router.navigate(['/paste', receivedPaste.hash])
       } else {
         this.generateErrorMessage()
@@ -42,5 +46,23 @@ export class AddPasteComponent implements OnInit {
     });
 
     this.error = "An error has occured."
+  }
+
+  onKey(event: KeyboardEvent) {
+    var keyCode = event.keyCode || event.which;
+
+    if (keyCode == 9) {
+      const pasteInput = this.pasteInput.nativeElement
+      var start = pasteInput.selectionStart
+      var end = pasteInput.selectionEnd
+
+      this.paste = this.paste.substring(0, start)
+        + "\t"
+        + this.paste.substring(end);
+
+      pasteInput.selectionStart =
+      pasteInput.selectionEnd = start + 1;
+      event.preventDefault();
+    }
   }
 }
