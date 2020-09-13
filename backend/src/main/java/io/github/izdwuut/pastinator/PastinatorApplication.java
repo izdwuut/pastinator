cbb3cf7aefb8440d8f811da44c50d30346960ac6
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-13 12:10:57 +0200

    Add backend bare bones.

---
+++ backend/src/main/java/io/github/izdwuut/pastinator/PastinatorApplication.java
@@ -0,0 +1,13 @@
+package io.github.izdwuut.pastinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastinatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/izdwuut/pastinator/PastinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.izdwuut.pastinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
