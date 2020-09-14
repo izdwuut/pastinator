52b9ed30d9d36475ad78e335856775bc3debbd5f
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-14 08:22:28 +0200

    Add AddPasteComponent.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddPasteComponent } from './add-paste/add-paste.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AddPasteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
