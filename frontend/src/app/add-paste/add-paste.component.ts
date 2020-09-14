14f46cb044f58cfa23e613ef3cf182c1c93c67d1
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-14 08:37:04 +0200

    Add RestService.

--- frontend/src/app/add-paste/add-paste.component.ts
+++ frontend/src/app/add-paste/add-paste.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Paste } from '../entities/paste';
+import { RestService } from '../services/rest.service';
 
 @Component({
   selector: 'app-add-paste',
   templateUrl: './add-paste.component.html',
   styleUrls: ['./add-paste.component.sass']
 })
-export class AddPasteComponent implements OnInit {
+export class AddPasteComponent {
+  getPasteSubscription: Subscription
+  constructor(private api: RestService) {
+    this.getPasteSubscription = this.api.getPaste('2bb8deb8f611558').subscribe((paste: Paste) => {
+      alert(paste.paste)
+    })
+   }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnDestroy() {
+    if(this.getPasteSubscription) {
+      this.getPasteSubscription.unsubscribe()
+    }
   }
 
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddPasteComponent } from './add-paste/add-paste.component';
 import { RestService} from './services/rest.service'
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +15,8 @@ import { RestService} from './services/rest.service'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [
     RestService
