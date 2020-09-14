2888ed06143270ff00303d6cbede74bd6deb57c5
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-14 08:30:39 +0200

    Add RestService.

---
+++ frontend/src/app/entities/paste.ts
@@ -0,0 +1,7 @@
+export interface Paste {
+    hash: String,
+    paste: String
+    author?: String,
+    title?: String,
+    expires?: Date
+}
---
+++ frontend/src/app/services/rest.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http'
+import {environment} from '../../environments/environment'
+import {Paste} from '../entities/paste'
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  apiUrl = environment.apiUrl
+  constructor(private http: HttpClient) { }
+
+  addPaste(paste: Paste): Observable<Paste> {
+    return this.http.post<Paste>(this.apiUrl + 'paste', paste).pipe()
+  }
+
+  getPaste(hash: String): Observable<Paste> {
+    return this.http.get<Paste>(this.apiUrl + 'paste/' + hash).pipe()
+  }
+}
