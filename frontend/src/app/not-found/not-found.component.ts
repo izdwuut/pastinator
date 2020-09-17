683e9789b3e574858a309ef49424f53c27cb7758
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-17 12:40:39 +0200

    Add NotFoundComponent logic.

--- frontend/src/app/not-found/not-found.component.ts
+++ frontend/src/app/not-found/not-found.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.sass']
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent {
 
-  constructor() { }
+  constructor(private router: Router) {
+    setTimeout(() => {
+      router.navigate([''])
+    }, 3000)
+   }
 
-  ngOnInit(): void {
-  }
 
 }
