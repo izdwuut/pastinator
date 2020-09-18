2bff652c1c9dcb71959efc0ca0907fc562cd68ca
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-18 07:54:41 +0200

    Add backend bare bones.

--- backend/src/main/java/io/github/izdwuut/pastinator/PastinatorApplication.java
+++ backend/src/main/java/io/github/izdwuut/pastinator/PastinatorApplication.java
@@ -2,12 +2,8 @@ package io.github.izdwuut.pastinator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@EntityScan(basePackages = "io.github.izdwuut.pastinator.model.entities")
 public class PastinatorApplication {
 
 	public static void main(String[] args) {
