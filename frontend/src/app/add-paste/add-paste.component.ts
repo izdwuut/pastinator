c0ae1ddcce52e1d73d7308f09091588b47133843
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-20 22:10:16 +0200

    Add AddPasteComponent.

---
+++ frontend/src/app/add-paste/add-paste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-paste',
+  templateUrl: './add-paste.component.html',
+  styleUrls: ['./add-paste.component.sass']
+})
+export class AddPasteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
