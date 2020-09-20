8a26a80832bd5e2d52b30f001dc6abdf8c38c385
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-20 05:50:56 +0200

    Add DatabaseConfig.

---
+++ backend/src/main/java/io/github/izdwuut/pastinator/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package io.github.izdwuut.pastinator.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Value("${DATABASE_URL}")
+    String dbUrl;
+    @Value("${DATABASE_USERNAME}")
+    String dbUsername;
+    @Value("${DATABASE_PASSWORD}")
+    String dbPassword;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(dbUsername);
+        config.setPassword(dbPassword);
+        return new HikariDataSource(config);
+    }
+}
