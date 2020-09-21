4693bbf918fd386748e6c1c9f4c27f1af1c437bb
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-21 08:52:33 +0200

    Add scheduling.

--- backend/src/main/java/io/github/izdwuut/pastinator/PastinatorApplication.java
+++ backend/src/main/java/io/github/izdwuut/pastinator/PastinatorApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EntityScan(basePackages="io.github.izdwuut.pastinator.model.entities")
+@EnableScheduling
 public class PastinatorApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/io/github/izdwuut/pastinator/model/repositories/PasteRepository.java
+++ backend/src/main/java/io/github/izdwuut/pastinator/model/repositories/PasteRepository.java
@@ -3,6 +3,9 @@ package io.github.izdwuut.pastinator.model.repositories;
 import io.github.izdwuut.pastinator.model.entities.Paste;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+
 public interface PasteRepository extends CrudRepository<Paste, String> {
     Paste findByHash(String hash);
+    Paste[] findByExpiresBefore(Date date);
 }
