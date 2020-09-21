a2adb1a6827a143f1d62182e7c36d5b63c29f14d
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-21 05:24:31 +0200

    Style AddPasteComponent.

--- frontend/src/app/add-paste/add-paste.component.ts
+++ frontend/src/app/add-paste/add-paste.component.ts
@@ -1,24 +1,12 @@
-import { Component} from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 import { RestService } from '../services/rest.service';
-import {Paste} from '../entities/paste'
+
 @Component({
   selector: 'app-add-paste',
   templateUrl: './add-paste.component.html',
   styleUrls: ['./add-paste.component.sass']
 })
-export class AddPasteComponent{
-  getPasteSubscription: Subscription
-  constructor(private api: RestService) { 
-    this.getPasteSubscription = this.api.getPaste('3b3d613679d1c98').subscribe((paste: Paste) => {
-      alert(paste.paste)
-    })
-  }
-
-
-  ngOnDestroy() {
-    if(this.getPasteSubscription) {
-      this.getPasteSubscription.unsubscribe()
-    }
+export class AddPasteComponent {
+  constructor(private api: RestService) {
   }
 }
