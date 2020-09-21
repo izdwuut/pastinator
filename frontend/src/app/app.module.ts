91dd67d925bc100ea28c2720295a6ae82f4ea85e
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-21 03:59:44 +0200

    Add AddPasteComponent markup.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddPasteComponent } from './add-paste/add-paste.component';
 import {RestService} from './services/rest.service'
 import { HttpClientModule } from '@angular/common/http';
+import {MatDatepickerModule} from '@angular/material/datepicker'
+import { FormsModule } from '@angular/forms';
+import {MatSnackBarModule} from '@angular/material/snack-bar'
+import {MatInputModule} from '@angular/material/input'
+import {MatIconModule} from '@angular/material/icon'
+import {MatMomentDateModule,MomentDateAdapter } from '@angular/material-moment-adapter'
+import * as _moment from 'moment'
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+const moment = _moment
+
+export const DATE_FORMATS = {
+  parse: {
+    dateInput: 'LL',
+    locale: 'en-GB'
+  },
+  display: {
+    dateInput: 'YYYY-MM-DD',
+    monthYearLabel: 'YYYY'
+  },
+  week: {
+    dow: 1
+  }
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,10 +40,20 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatDatepickerModule,
+    MatIconModule,
+    FormsModule,
+    MatMomentDateModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [
-    RestService
+    RestService,
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: DATE_FORMATS},
+    {provide: MAT_DATE_LOCALE, useValue: DATE_FORMATS.parse.locale}
   ],
   bootstrap: [AppComponent]
 })
