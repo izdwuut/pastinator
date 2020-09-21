a0008c570f15e2a4858b4be569562a645dc9117f
Bartosz izdwuut Konikiewicz <izdwuut@gmail.com>
2020-09-21 03:23:54 +0200

    Add RestService.

--- frontend/src/app/add-paste/add-paste.component.ts
+++ frontend/src/app/add-paste/add-paste.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { RestService } from '../services/rest.service';
+import {Paste} from '../entities/paste'
 @Component({
   selector: 'app-add-paste',
   templateUrl: './add-paste.component.html',
   styleUrls: ['./add-paste.component.sass']
 })
-export class AddPasteComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class AddPasteComponent{
+  getPasteSubscription: Subscription
+  constructor(private api: RestService) { 
+    this.getPasteSubscription = this.api.getPaste('3b3d613679d1c98').subscribe((paste: Paste) => {
+      alert(paste.paste)
+    })
   }
 
+
+  ngOnDestroy() {
+    if(this.getPasteSubscription) {
+      this.getPasteSubscription.unsubscribe()
+    }
+  }
 }
